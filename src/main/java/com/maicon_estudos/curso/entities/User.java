429f6386bcb567a71db4c34a061bfcd64d1dee09
maicon-concurso <pvhservi@gmail.com>
2024-09-05 17:34:53 -0400

    Project created

---
+++ src/main/java/com/maicon_estudos/curso/entities/User.java
@@ -0,0 +1,93 @@
+package com.maicon_estudos.curso.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+// Declaração da classe pública "User". Esta classe será usada para representar os dados de um usuário.
+	
+	private static final long serialVersionUID = 1L;
+
+	private Long id; 
+	// Atributo privado "id" do tipo Long, que provavelmente será o identificador único do usuário no banco de dados.
+	
+	private String name; 
+	private String email; 
+	private String phone; 	
+	private String password; 
+
+	public User() {
+		// Construtor padrão (sem parâmetros), usado quando não se quer inicializar a classe com valores iniciais.
+	}
+
+	public User(long id, String name, String email, String phone, String password) {
+		super(); 
+		// Chama o construtor da classe pai (neste caso, Object). 
+		// No entanto, como User não está herdando nenhuma outra classe específica, não haveria necessidade explícita de chamar super().
+		
+		this.id = id;
+		// Inicializa o atributo "id" com o valor passado como parâmetro.
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
