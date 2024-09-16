255e656bd77f5d108033667f22a4e9d945e17f5e
maicon-concurso <pvhservi@gmail.com>
2024-09-16 17:36:57 -0400

    271. Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

--- src/main/java/com/maicon_estudos/curso/entities/User.java
+++ src/main/java/com/maicon_estudos/curso/entities/User.java
@@ -1,12 +1,15 @@
 package com.maicon_estudos.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,9 @@ public class User implements Serializable {
 	private String email; 
 	private String phone; 	
 	private String password; 
+	
+	@OneToMany(mappedBy ="client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 		// Construtor padrão (sem parâmetros), usado quando não se quer inicializar a classe com valores iniciais.
@@ -41,6 +47,7 @@ public class User implements Serializable {
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+		
 	}
 
 	public Long getId() {
@@ -87,7 +94,11 @@ public class User implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -99,5 +110,7 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 }
