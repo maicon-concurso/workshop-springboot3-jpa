2024cfc5cf1a4d0e4ea104cd07ab4a1e915ab1e0
maicon-concurso <pvhservi@gmail.com>
2024-10-22 15:56:37 -0400

    280. Entidade Payment, associação um para um

--- src/main/java/com/maicon_estudos/curso/config/TestConfig.java
+++ src/main/java/com/maicon_estudos/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.maicon_estudos.curso.entities.Category;
 import com.maicon_estudos.curso.entities.Order;
 import com.maicon_estudos.curso.entities.OrderItem;
+import com.maicon_estudos.curso.entities.Payment;
 import com.maicon_estudos.curso.entities.Product;
 import com.maicon_estudos.curso.entities.User;
 import com.maicon_estudos.curso.entities.enums.OrderStatus;
@@ -83,5 +84,10 @@ public class TestConfig  implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
