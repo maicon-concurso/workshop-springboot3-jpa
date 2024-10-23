d303f273e0daaf9ba589639d1ab2df8597a64632
maicon-concurso <pvhservi@gmail.com>
2024-10-23 16:59:44 -0400

    282. Inserção de User

--- src/main/java/com/maicon_estudos/curso/repositories/OrderItemRepository.java
+++ src/main/java/com/maicon_estudos/curso/repositories/OrderItemRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.maicon_estudos.curso.entities.OrderItem;
 
+import jakarta.persistence.Id;
+
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 
--- src/main/java/com/maicon_estudos/curso/resources/UserResource.java
+++ src/main/java/com/maicon_estudos/curso/resources/UserResource.java
@@ -1,37 +1,49 @@
 package com.maicon_estudos.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.maicon_estudos.curso.entities.User;
 import com.maicon_estudos.curso.services.UserService;
 
-@RestController  // Indica que esta classe é um controlador REST, responsável por tratar requisições HTTP.
-@RequestMapping(value="/users/")  // Define a URL base que serhttps://desktop.postman.com/?desktopVersion=11.11.0&userId=38133108&teamId=0&region=usá usada para acessar os endpoints deste controlador. Neste caso, /users.
+@RestController  
+@RequestMapping(value="/users/") 
 public class UserResource {
 
 	@Autowired
 	private UserService service;
 	
-    @GetMapping  // Mapeia requisições GET (quando o cliente pede dados) para o método abaixo.
+    @GetMapping  
     public ResponseEntity<List<User>> findAll() {
     	
     	List<User> list = service.findAll();
 
-        // Retorna uma resposta HTTP 200 (OK) com o objeto User no corpo da resposta.
-        // ResponseEntity é uma classe do Spring usada para personalizar a resposta HTTP.
+       
         return ResponseEntity.ok(list);
     }
+    
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
     	User obj = service.findById(id);
     	return ResponseEntity.ok().body(obj);
+    }
+    
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+    	obj = service.insert(obj);
+    	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+    			.buildAndExpand(obj.getId()).toUri();
+    	return ResponseEntity.created(uri).body(obj);
     }
     
 }
